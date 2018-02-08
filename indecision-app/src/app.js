58c98cdde57c007e082905f0f5c4128cfda662e3
Vikas Kumar <negi.vikash925@gmail.com>
2018-02-08 23:05:07 +0530

    first React Component

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -1,59 +1,14 @@
-
-// JSX - Javascript XML
-
-
-const app = {
-    title:'Indecision App',
-    subtitle:'Put your life in the hands of computer',
-    options:['One', 'Two'],
-};
-const onFormSubmit = (e) => {
-    e.preventDefault();
-    const option = e.target.elements.option.value;
-    if (option){
-        app.options.push(option);
-        e.target.elements.option.value ='';
-        render();
+class Header extends React.Component {
+    render(){
+        return <p>This is from header</p>;
     }
 }
-
-const onRemoveAll = () => {
-    app.options =[];
-    render();
-
-};
-const onMakeDecision = () => {
-    const randomNum = Math.floor(Math.random() * app.options.length);
-    const option = app.options[randomNum];
-    alert(option);
-};
-const appRoot = document.getElementById("app");
-//create "Remove All" button above list
-const render = () => {
-    const template = (
-        <div>
-            <h1>{app.title}</h1>
-            {app.subtitle && <p>{app.subtitle}</p>}
-            <p>{app.options.length > 0 ? 'Here are your options':'No options'}</p>
-            <p>{app.options.length}</p>
-            <button disabled ={app.options.length === 0} onClick={onMakeDecision}>What Should I do?</button>
-            <button onClick={onRemoveAll}>Remove All</button>
-                {   /*numbers.map((number) => {
-                    return <p key ={number}>Number :{number}</p> ;
-                })*/
-
-            }
-            <ol>
-                {
-                    app.options.map((text) => <li key={text}>{text}</li>)
-                }
-            </ol>
-            <form onSubmit={onFormSubmit}>
-                <input type="text" name="option" />
-                <button>Add Options</button>
-            </form>
-        </div>
-    );
-    ReactDOM.render(template, appRoot);
-};
-render();
+const jsx = (
+    <div>
+        <h1>Title</h1>
+        <Header />
+        <Header />
+        <Header />
+    </div>
+);
+ReactDOM.render(jsx, document.getElementById('app'));
