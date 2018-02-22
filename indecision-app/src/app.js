3c9605ce512eab548fef9c7355c7d981e0a96258
Vikas Kumar <negi.vikash925@gmail.com>
2018-02-22 22:00:38 +0530

    Introduction to 3rd party components and passing children to Component

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -2,4 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import IndecisionApp from './components/IndecisionApp';
 
-ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
+const Layout = (props) => {
+    return (
+        <div>
+            <p>Header</p>
+            {props.children}
+            <p>footer</p>
+        </div>
+    );
+};
+
+
+// ReactDOM.render((
+//     <Layout>
+//         <div>
+//             <h1>Page title</h1>
+//             <p>This is my Page</p>
+//         </div>
+//     </Layout>
+// ),document.getElementById('app'));
+ReactDOM.render(<IndecisionApp />,document.getElementById('app'));
