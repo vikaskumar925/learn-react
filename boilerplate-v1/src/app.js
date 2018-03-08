98e0d58202ad6e0f9d8122e1f35d9d0dfcc79ea2
Vikas Kumar <negi.vikash925@gmail.com>
2018-03-08 22:37:56 +0530

    Add the new Project folder "Boiler Plate"

---
+++ boilerplate-v1/src/app.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import "normalize.css/normalize.css";
+import './styles/styles.scss';
+
+ReactDOM.render(<p>this is my test</p>,document.getElementById('app'));
