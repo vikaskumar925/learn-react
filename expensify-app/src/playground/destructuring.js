3712b4f2e3cf86bfa39143588832fe8c82b238cf
Vikas Kumar <negi.vikash925@gmail.com>
2018-03-11 20:27:09 +0530

    ES6 Array Destructuring

--- expensify-app/src/playground/destructuring.js
+++ expensify-app/src/playground/destructuring.js
@@ -1,3 +1,5 @@
+//Object destructuring
+
 // const person = {
 //     name:'Vikas',
 //     age :25,
@@ -15,13 +17,24 @@
 //     console.log(`It's ${temperature} in ${city}`);
 // }
 
-const book = {
-    title:'Ego is the Enemy',
-    author:'Ryan Holiday',
-    publisher:{
-        name:'Penguin',
-    },
-};
+// const book = {
+//     title:'Ego is the Enemy',
+//     author:'Ryan Holiday',
+//     publisher:{
+//         name:'Penguin',
+//     },
+// };
+//
+// const {name:publisherName = 'Self-Published'} = book.publisher;
+// console.log(publisherName);
 
-const {name:publisherName = 'Self-Published'} = book.publisher;
-console.log(publisherName);
+//Array destructuring
+
+// const address = ['1299 S Juniper Street', 'Philadelphia', 'Pennsylvania', '19147'];
+//
+// const [, city, state='New York'] = address;
+// console.log(`You are in ${city} ${state}`);
+
+const item = ['Coffee (ice)', '$2.00', '$2.80', '2.75'];
+const [itemName, , cost,] = item;
+console.log(`A ${itemName} costs ${cost}`);
